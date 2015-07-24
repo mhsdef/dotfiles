00856b4106374e52d116c3f82dbafd81cd1a8d22
Hew <hewsut@gmail.com>
2015-07-23 22:22:52 -0400

    Add neophyte fishshell config

---
+++ fish/config.fish
@@ -0,0 +1,13 @@
+set -U EDITOR vim
+set -g fish_key_bindings fish_vi_key_bindings
+
+alias phpgrep='find . -type f -name "*.php" -print0 | xargs -0 grep -n $1'
+alias jsgrep='find . -type f -name "*.js" -print0 | xargs -0 grep -n $1'
+
+alias c='cd ~/code'
+alias d='cd ~/Downloads'
+alias dt='cd ~/Desktop'
+alias mc='cd ~/code/automattic-sandbox/missioncontrol/public_html'
+alias uni='unison -ui text -repeat watch automattic-sandbox'
+alias wpcom='cd ~/code/automattic-sandbox/wpcom/public_html'
+alias www='cd ~/vagrant-local/www'
