f900366a9a9512a793fb4078c3f7ffe19365b51f
Hew <hewsut@gmail.com>
2015-07-27 13:03:10 -0400

    Clear out default fish greeting, jsgrep .jsx files too

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,9 @@
 set -U EDITOR vim
+set -g -x fish_greeting ''
 set -g fish_key_bindings fish_vi_key_bindings
 
 alias phpgrep='find . -type f -name "*.php" -print0 | xargs -0 grep -n $1'
-alias jsgrep='find . -type f -name "*.js" -print0 | xargs -0 grep -n $1'
+alias jsgrep='find . -type f -name "*.js" -o -name "*.jsx" -print0 | xargs -0 grep -n $1'
 
 alias c='cd ~/code'
 alias d='cd ~/Downloads'
