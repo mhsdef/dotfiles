0b1d05323b4b80c6aba27cac025f1c5f0a30208d
Hew <hewsut@gmail.com>
2015-08-12 22:31:46 -0400

    Fix jsgrep in config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -3,7 +3,7 @@ set -g -x fish_greeting ''
 set -g fish_key_bindings fish_vi_key_bindings
 
 alias phpgrep='find . -type f -name "*.php" -print0 | xargs -0 grep -n $1'
-alias jsgrep='find . -type f -name "*.js" -o -name "*.jsx" -print0 | xargs -0 grep -n $1'
+alias jsgrep='find . -type f \( -name "*.js" -o -name "*.jsx" \) -print0 | xargs -0 grep -n $1'
 
 alias c='cd ~/code'
 alias d='cd ~/Downloads'
