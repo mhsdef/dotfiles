2fc04d4abdb11ad31c92be208ca6ce4e3bb2c40e
Hew <github@mhs.fastmail.com>
2016-01-18 14:04:31 -0500

    Add exgrep alias for searching Elixir file types

--- fish/config.fish
+++ fish/config.fish
@@ -4,6 +4,7 @@ set -g fish_key_bindings fish_vi_key_bindings
 
 alias phpgrep='find . -type f -name "*.php" -print0 | xargs -0 grep -n $1'
 alias jsgrep='find . -type f \( -name "*.js" -o -name "*.jsx" \) -print0 | xargs -0 grep -n $1'
+alias exgrep='find . -type f \( -name "*.ex" -o -name "*.exs" \) -print0 | xargs -0 grep -n $1'
 
 alias c='cd ~/code'
 alias d='cd ~/Downloads'
