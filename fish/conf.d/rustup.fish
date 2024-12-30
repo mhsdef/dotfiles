a7bccdc234fe0ed4112cb565043f92c1e5534a63
Matt Sutton <git@mhs.fastmail.com>
2024-12-30 17:22:18 -0500

    Add rust pathing

---
+++ fish/conf.d/rustup.fish
@@ -0,0 +1,6 @@
+# rustup shell setup
+if not contains "{cargo_bin}" $PATH
+    # Prepending path in case a system-installed rustc needs to be overridden
+    set -x PATH "{cargo_bin}" $PATH
+end
+source "$HOME/.cargo/env.fish"
